eceae2e1415d9d3f1f664fd56d0697d80f7dc7bd
Rubal Walia <walia.rubal@gmail.com>
2025-11-03 20:11:41 -0800

    Added standard toolbar.

---
+++ Brody.Client/Views/StandardToolbarView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Brody.Client.Views;
+
+public partial class StandardToolbarView : UserControl
+{
+    public StandardToolbarView()
+    {
+        InitializeComponent();
+    }
+}
